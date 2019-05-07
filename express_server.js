f9edb4ce7155d2adebaeda58fcc1ea42124fd2d0
David Eastmond <davebuck1981@gmail.com>
2019-05-07 16:32:05 +0000

    added an HTML response

--- express_server.js
+++ express_server.js
@@ -14,7 +14,11 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
+// Add a route
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
